7efd3098164008cf3546e783dd9b81502e87ce19
byteh <hank.han@cloudwise.com>
2017-05-12 03:41:52 +0800

    printfmt.py

---
+++ printfmt.py
@@ -0,0 +1,12 @@
+language=7
+age=27
+rice="rice"
+print("Language %s: I am Python. What's for supper? %s haha."%(language,rice))
+print ("His name is %s"%("Aviad"))
+print ("He is %d years old"%age)
+print ("His height is %f m"%(1.83))
+print ("His height is %.2f m"%(1.83))
+#指定占位符宽度
+print ("Name:%10s Age:%8d Height:%8.2f"%("Aviad",25,1.83))
+#指定占位符宽度（左对齐）
+print ("Name:%-10s Age:%-8d Height:%-8.2f"%("Aviad",25,1.83))
