7ee6ce158d50b45048db32f421f8a2c6eb35743e
hanyubin <byteh@163.com>
2017-07-31 22:28:19 +0800

    元组&&切片

---
+++ tuple1-4.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#coding:utf-8
+
+#元组 && 切片
+a = (1,3,3,4,5,6,7,8,9,10)
+print a[1]
+print a[1:]
+print a[1:3]
+print a[1:8:2]
+print a[:-1]
+print a[::2]
+print a[::-1]
+print min(a),max(a)
