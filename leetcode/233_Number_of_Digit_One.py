4f112d3f4b87500d4ca51124e8b927a6940f3810
byteh <hank.han@cloudwise.com>
2017-09-04 20:19:29 +0800

    233_Number_of_Digit_One.py 提交时仍旧报Memory Limit Exceeded Last executed input: 3184191

--- leetcode/233_Number_of_Digit_One.py
+++ leetcode/233_Number_of_Digit_One.py
@@ -9,15 +9,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        result = 0;
-        list = []
+        result = 0
+        if n < 1:
+            return 0
+        # dict = {}
         for i in range(1,n+1):
             numberStr = str(i)  #类型转换
-            hasDigitOne = numberStr.find('1') #字符串查找
-            if hasDigitOne >= 0:
-                result += 1
-                list.append(i)
-        print list
+            postion = 0
+            iCnt = 0
+            while postion >= 0:
+                postion = numberStr.find('1',postion) #字符串查找
+                if(postion >=0):
+                    postion +=1
+                    # print numberStr,postion
+                    iCnt += 1
+            if iCnt > 0 :
+                result += iCnt
         return result
 
 number = int(raw_input('输入一个int类型的数：'))
